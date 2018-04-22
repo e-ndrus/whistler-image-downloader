ce1ea630285a76f8cb0a0d5439252376467959f2
e-ndrus <andrei.khrapavitski@gmail.com>
2018-04-22 10:56:46 +0300

    Reworked for more general use

--- Whistler Image Downloader.py
+++ Whistler Image Downloader.py
@@ -1,9 +1,21 @@
+"""
+Created on Sun Apr 22 10:54:35 2018
+
+@author: Andrei Khrapavitski
+"""
+
 import os.path
 import urllib.request
 import pandas as pd
+import re
 
+dataset = pd.read_csv('unformatted.csv')
 
-dataset = pd.read_csv('links2.csv')
+def urlClean(vert, hor):
+    url = dataset.iloc[vert, hor]
+    url = url.replace(';', '')
+    url = url.replace('https://whistlerapp.org/attachments/', 'https://whistlerapp.org/xmedias/download/id/')
+    return url
 
 li=[]
 name=[]
@@ -11,18 +23,14 @@ name=[]
 import pathlib
 pathlib.Path('downloads3').mkdir(parents=True, exist_ok=True) 
 
-def isNaN(num):
-    return num != num
-
 for a in range(0,len(dataset.index)):
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', urlClean(a,5))
         
-    for i in range(5,11):
-        fileLocation = dataset.iloc[a,i]
-        if isNaN(fileLocation) == False:
-            li.append(fileLocation)
-            Const = dataset.iloc[a,1]  
-            StationNum = str(dataset.iloc[a,4])
-            name.append(Const+' Участок №'+StationNum)
+    for i in range(len(urls)):
+        li.append(urls[i])
+        Const = dataset.iloc[a,1]  
+        StationNum = str(dataset.iloc[a,4])
+        name.append(Const+' Участок №'+StationNum)
 
 for a in range(len(li)):    
     filename = os.path.join('downloads3',str(a)+' '+name[a]+'.jpg')
@@ -32,5 +40,5 @@ for a in range(len(li)):
         urllib.request.urlretrieve(li[a], filename)
     except Exception as inst:
         print(inst)
-        print('  Encountered unknown error. Continuing.') 
+        print('  An error occured. Continuing.') 
         
